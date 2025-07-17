cf5c624190146c63b35a8ca39c9a4f78482a1df9
MichaelFarid <michael.farid.999@gmail.com>
2025-07-16 23:54:37 -0400

    Update command_line.py

--- virnatrap/command_line.py
+++ virnatrap/command_line.py
@@ -1,20 +1,20 @@
 import sys
 import os
-from os.path import isdir,isfile
+from os.path import isdir, isfile
 from .virnatrap import run_virna_pred
 import argparse
 import virnatrap
 
 # Constants ------------------------------------------------------------------------------------------------------------
-
-DESCRIPTION = "Extract viral contigs from a directory with unmapped RNAseq reads fastq files and saves a file with contigs for each fastq in an output directory"
-#PWD = os.getcwd()
+DESCRIPTION = (
+    "Extract viral contigs from a directory with unmapped RNAseq reads fastq files "
+    "and saves a file with contigs for each fastq in an output directory"
+)
 mpath = virnatrap.__file__
 PWD = mpath[:-12]
 
 # Terminal functions ---------------------------------------------------------------------------------------------------
 def virnatrap_predict():
-
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument(
         "--input", type=str, help="input directory", required=True
@@ -23,61 +23,67 @@ def virnatrap_predict():
         "--output", type=str, help="output directory", required=True
     )
     parser.add_argument(
-        "--fastmode", type=int, help="1 to run in fastmode and call C function to assemble reads", required=False
+        "--fastmode", type=int,
+        help="1 to run in fastmode and call C function to assemble reads",
+        required=False,
+        default=0,
     )
     parser.add_argument(
-        "--multi_proc", type=int, help="run with pool multi processing, if many input files", required=False
+        "--multi_proc", type=int,
+        help="run with pool multi processing, if many input files",
+        required=False,
+        default=1,
     )
     parser.add_argument(
         "--num_threads", type=int,
         help="number of threads to run with pool multi processing",
-        required=False
+        required=False,
+        default=48,
     )
     parser.add_argument(
-        "--model_path", type=int, help="path to Tensorflow model to predict whether reads of a fixed length come from viruses or not", required=False
+        "--model_path", type=str,
+        help="path to Tensorflow model to predict whether reads come from viruses",
+        required=False,
+    )
+    parser.add_argument(
+        "--dump_seeds", action="store_true",
+        help="write raw 48bp seeds to a separate *_seeds.txt file",
     )
 
     args = parser.parse_args()
 
     inpath = args.input
     outpath = args.output
+    fastmode = bool(args.fastmode)
+    multi_proc = bool(args.multi_proc)
+    num_threads = args.num_threads
+    dump_seeds = args.dump_seeds
 
     if not isdir(inpath):
-        print('input directory %s not found',inpath)
+        print(f"input directory {inpath} not found")
         sys.exit(1)
-
     if not isdir(outpath):
-        print('output directory %s not found',outpath)
+        print(f"output directory {outpath} not found")
         sys.exit(1)
 
-
-    if args.fastmode is None:
-        fastmode = False
+    if args.model_path and isfile(args.model_path):
+        model_path = args.model_path
     else:
-        fastmode = args.fastmode
+        if args.model_path:
+            print(f"model {args.model_path} not found, using default viRNAtrap model")
+        model_path = os.path.join(PWD, 'model', 'model_lr_0.005_pool_5_emb_25_l2_0.02_64.hdf5')
 
-    if args.num_threads is None:
-        num_threads = 48
-    else:
-        num_threads = args.num_threads
-
-    if args.multi_proc is None:
-        multi_proc = True
-    else:
-        multi_proc = args.multi_proc
-
-
-    if args.model_path is None:
-        
-        model_path = PWD+'/model/model_lr_0.005_pool_5_emb_25_l2_0.02_64.hdf5'
-    else:
-        if not isfile(args.model_path):
-            print('model %s not found, using default viRNAtrap model', args.model_path)
-            model_path = PWD + '/model/model_lr_0.005_pool_5_emb_25_l2_0.02_64.hdf5'
-        else:
-            model_path = args.model_path
-
-
-    print("Reading fastq at {}...".format(inpath))
-    run_virna_pred(inpath, outpath, fastmode, multi_proc, model_path,num_threads)
+    print(f"Reading fastq at {inpath}...")
+    run_virna_pred(
+        inpath,
+        outpath,
+        fastmode,
+        multi_proc,
+        model_path,
+        num_threads,
+        dump_seeds
+    )
     print("done")
+
+if __name__ == '__main__':
+    virnatrap_predict()
